2358af6231dba160ffceca059f697ccfc31dd06d
Josuel Beserra Mroczko <josuelbeserramroczko@gmail.com>
2025-02-11 22:35:08 -0300

    backAndCriado
    criei o banckend e conectei ao sql usando xampp

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const db = require('./db'); 
+const app = express();
+const PORT = process.env.PORT || 5000; 
+
+app.use(cors({
+    origin:"http://localhost:3000",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders:['Content-Type', 'Authorization']
+}));
+
+app.use(bodyParser.json());
+
+
+
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
