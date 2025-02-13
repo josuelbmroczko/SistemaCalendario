4d0dc53ac012ed26c6831eef5decf876fb60230d
Josuel Beserra Mroczko <josuelbeserramroczko@gmail.com>
2025-02-13 08:05:56 -0300

    temaEscuroEClaro
    adicionei um evento no app.js para poder mudar de tem escuro para claro usando a className escuro/claro comuseEffect

--- front-calendario/src/App.js
+++ front-calendario/src/App.js
@@ -1,15 +1,34 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Corrigido o import
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  
 import CalendarioEventos from './Componentes/calendario/CalendarioEventos';
 import Home from './Componentes/Home/Home';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+
+  const [modoEscuro, setModoEscuro] = useState(false);
+
+  useEffect(() => {
+    if (modoEscuro) {
+      document.body.className = 'escuro';
+    } else {
+      document.body.className = 'claro';
+    }
+  }, [modoEscuro]);
+
+
   return (
     <Router>   
-      <Routes>   
-      <Route path="/" element={<Home />} />  
-        <Route path="/CalendarioEventos" element={<CalendarioEventos />} />  
-      </Routes>
+      <div>
+        <button className='botaoQueTrocarOTema' onClick={() => setModoEscuro(prevModo => !prevModo)}>
+          Mudar para {modoEscuro ? 'claro' : 'escuro'}
+        </button>
+        <Routes>   
+          <Route path="/" element={<Home />} />  
+          <Route path="/CalendarioEventos" element={<CalendarioEventos />} />  
+        </Routes>
+      </div>
     </Router>   
   );
 }
---
+++ front-calendario/src/Componentes/estilos/temaClaroOuEscuro.js
@@ -0,0 +1,15 @@
+const estilos = {
+    secao1: {
+      fundo: "#ffffff",  
+      texto: "#000000",  
+    },
+
+    topo: {
+      fundo: "#121212",  
+      texto: "#ffffff",   
+
+    },
+  };
+  
+  export default estilos;
+  
